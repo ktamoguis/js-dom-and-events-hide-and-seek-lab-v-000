8303c47549650a025f96c23ed80529bf43093b4e
ktamoguis <ktamoguis@gmail.com>
2018-08-17 16:48:49 +0800

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('div#nested div div div div.target');
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + n);
+  }
+}
+
+function deepestChild(){
+  //return document.querySelector('#grand-node div div div div');
+  let returnElement = '#grand-node div'
+  const lis = document.querySelectorAll('#grand-node div')
+
+  for (let i = 1; i < lis.length; i++) {
+    returnElement = returnElement + ' div'
+  }
+  return document.querySelector(returnElement);
+
+}
